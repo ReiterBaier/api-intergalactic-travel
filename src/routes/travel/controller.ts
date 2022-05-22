2e33a70fa0c734e12d6eecc6c809f2c9943de868
Reiter Baier <91735552+ReiterBaier@users.noreply.github.com>
2022-05-22 18:37:21 -0300

    Update controller.ts

--- src/routes/travel/controller.ts
+++ src/routes/travel/controller.ts
@@ -21,26 +21,3 @@ export const createTravel = function() {server.post('/travel', async (req, res) 
 
 
 
-/*const mainBody = []
-
-export const postTravel = function () { server.post('/travel' , async (req, res) => {
-  const {
-          pilot,
-          copilot,
-          starship,
-          homePlanet,
-          destionationPlanet  }   = req.body;
-
-  const objectBody = {
-    pilot,
-    copilot,
-    starship,
-    homePlanet,
-    destionationPlanet
-  }
-
-  mainBody.push(objectBody)
-
-  return res.status(201).json(objectBody)
-})
-}*/
