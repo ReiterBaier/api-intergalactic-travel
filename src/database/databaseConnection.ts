c9f823d8e691ac67979537fa8d944004cbddec7d
Reiter Baier <91735552+ReiterBaier@users.noreply.github.com>
2022-05-22 17:39:26 -0300

    Criando banco de dados

---
+++ src/database/databaseConnection.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+export const connectDb = () => {
+    mongoose.connect('mongodb+srv://SYSDBA:masterkey@travels.j3i1q.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+   )
+   .then(()=> {console.log('Successful database connection')})
+   .catch((err) => console.log(err))
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { server } from './server'
+import { connectDb } from './database/databaseConnection'
 
+connectDb()
 server.listen(10550, () => console.log('Server is running on port 10550'))
 
 
