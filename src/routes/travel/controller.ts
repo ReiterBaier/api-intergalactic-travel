e2f397b65ec52b4d9012fd2e3df1fb7498700612
Reiter Baier <91735552+ReiterBaier@users.noreply.github.com>
2022-05-22 22:40:39 -0300

    Update controller.ts

--- src/routes/travel/controller.ts
+++ src/routes/travel/controller.ts
@@ -1,5 +1,6 @@
 import { travelModel } from '../../models/travel'
 
+/* Função para criação de novas viagens */
 export const createTravel = async (req, res) => {
   try {
     const travel = new travelModel(req.body) /* Espera que o body da requisição seja de acordo com o modelo de body */
@@ -10,11 +11,11 @@ export const createTravel = async (req, res) => {
   }
 }
 
-
+/* Função para busca de todas as viagens já realizadas */
 export const getTravel = async (req, res) => {
   try {
-    const travelList = await travelModel.find()
-    return res.render(travelList)
+    const travelList = await travelModel.find() /* Chama a função find do mongoose que lista um array de dados do banco */
+    return res.send(travelList) /* retorna o array de dados no response */
   } catch(error) {
     res.status(500).send({error: error.message})
   }
